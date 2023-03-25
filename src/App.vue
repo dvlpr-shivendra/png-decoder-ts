ec26a28506b2293d1e43053b3e63379b17fa4e13
Shivendra TechSter <shivendratechster@gmail.com>
2023-03-25 19:01:19 +0530

    WIP: unfiter PNG data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="container">
     <h1>World's best PNG decoder</h1>
+
+    <canvas ref="canvas"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, type Ref } from "vue";
 import PNGDecoder from "./PNGDecoder";
-fetch('http://localhost:5173/PNGs/1x1.png')
+
+type CanvasRef = HTMLCanvasElement | null;
+const canvas: Ref<CanvasRef> = ref(null)
+
+fetch('http://localhost:5173/PNGs/1x2.png')
   .then(response => response.blob())
   .then(data => data.arrayBuffer())
   .then(buffer => {
     const data = new Uint8Array(buffer);
     const decoder = new PNGDecoder(data);
     decoder.decodeBitmap();
+
+    if (!canvas.value) return;
+
+    decoder.draw(canvas.value);
   });
 </script>
--- src/PNGDecoder.ts
+++ src/PNGDecoder.ts
@@ -22,6 +22,11 @@ enum InterlaceMethod {
     Adam7,
 }
 
+interface Scanline {
+    filter: FilterType
+    data: Uint8Array
+}
+
 class PNGDecoder {
     private streamer: Streamer;
     private height: number = 0;
@@ -33,6 +38,8 @@ class PNGDecoder {
     private interlaceMethod: InterlaceMethod = InterlaceMethod.None;
 
     private compressedData: Uint8Array;
+
+    private scanlines: Scanline[] = [];
 
     constructor(data: Uint8Array) {
         this.streamer = new Streamer(data);
@@ -52,8 +59,8 @@ class PNGDecoder {
 
     decodeBitmap() {
         this.decodeChunks();
-
-        const decompressedData = pako.deflate(this.compressedData);
+        
+        const decompressedData = pako.inflate(this.compressedData);
 
         if (!this.interlaceMethod) this.decodeBitmapSimple(decompressedData);
         else throw new Error("Implement Adam7 PNG support.");
@@ -115,19 +122,85 @@ class PNGDecoder {
     }
 
     decodeBitmapSimple(data: Uint8Array) {
-        let scanlines = [];
-
         const streamer = new Streamer(data);
 
         for (let i = 0; i < this.height; i++) {
             const filter: FilterType = streamer.read();
             const scanlineSize = this.computeScanlineSizeForWidth(this.width);
-            scanlines.push({ filter, data: streamer.readBytes(scanlineSize) });
+            this.scanlines.push({ filter, data: streamer.readBytes(scanlineSize) });
         }
 
-        console.log(scanlines);
-
+        this.unfilter();
     }
+
+    unfilter() {
+        const bytesPerScanline = this.scanlines[0].data.length;
+        const bytesPerCompletePixel = Math.ceil((this.bitDepth + 7) / 8) * this.channels;
+
+        for (let i = 0; i < this.scanlines.length; i++) {
+            const scanline = this.scanlines[i];
+            if (scanline.filter === FilterType.None) {
+                continue;
+            } else if (scanline.filter === FilterType.Sub) {
+                for (let j = bytesPerCompletePixel; j < bytesPerScanline; j++) {
+                    const left = scanline.data[j - bytesPerCompletePixel];
+                    scanline.data[j] += left;
+                }
+            } else if (scanline.filter === FilterType.Up) {
+                for (let j = 0; j < bytesPerScanline; j++) {
+                    const above = i > 0 ? this.scanlines[i - 1].data[j] : 0;
+                    scanline.data[j] += above;
+                }
+            } else if (scanline.filter === FilterType.Average) {
+                for (let j = 0; j < bytesPerScanline; j++) {
+                    const left = j < bytesPerCompletePixel ? 0 : scanline.data[j - bytesPerCompletePixel];
+                    const above = i > 0 ? this.scanlines[i - 1].data[j] : 0;
+                    const average = Math.floor((left + above) / 2);
+                    scanline.data[j] += average;
+                }
+            } else if (scanline.filter === FilterType.Paeth) {
+                for (let j = 0; j < bytesPerScanline; j++) {
+                    const left = j < bytesPerCompletePixel ? 0 : scanline.data[j - bytesPerCompletePixel];
+                    const above = i > 0 ? this.scanlines[i - 1].data[j] : 0;
+                    const upperLeft = j < bytesPerCompletePixel || i < 1 ? 0 : this.scanlines[i - 1].data[j - bytesPerCompletePixel];
+                    const predictor = left + above - upperLeft;
+                    const predictorLeft = Math.abs(predictor - left);
+                    const predictorAbove = Math.abs(predictor - above);
+                    const predictorUpperLeft = Math.abs(predictor - upperLeft);
+                    let nearest = 0;
+                    if (predictorLeft <= predictorAbove && predictorLeft <= predictorUpperLeft) {
+                        nearest = left;
+                    } else if (predictorAbove <= predictorUpperLeft) {
+                        nearest = above;
+                    } else {
+                        nearest = upperLeft;
+                    }
+                    scanline.data[j] += nearest;
+                }
+            }
+        }
+    }
+
+
+    draw(canvas: HTMLCanvasElement) {
+        const ctx = canvas.getContext("2d");
+
+        if (!ctx) throw new Error("Could not get context");
+
+        this.scanlines.forEach((scanline, y) => {
+            const streamer = new Streamer(scanline.data);
+
+            for (let x = 0; x < this.width; x++) {
+                const r = streamer.read();
+                const g = streamer.read();
+                const b = streamer.read();
+
+                ctx.fillStyle = `rgb(${r},${g},${b})`;
+                ctx.fillRect(x, y, 1, 1);
+            }
+        });
+    }
+
 }
 
 export default PNGDecoder;
